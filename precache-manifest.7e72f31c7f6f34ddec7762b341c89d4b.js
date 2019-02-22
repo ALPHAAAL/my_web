7f7b4a42749b82c7e65e2059595524cb295b519e
Alex <alexlau@benovelty.com>
2019-02-22 08:48:06 +0800

    Updates

---
+++ precache-manifest.7e72f31c7f6f34ddec7762b341c89d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "40bacd71a3923f11c248",
+    "url": "/my_web/static/js/runtime~main.40bacd71.js"
+  },
+  {
+    "revision": "2415576c2c544ec36806",
+    "url": "/my_web/static/js/main.2415576c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/my_web/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "2415576c2c544ec36806",
+    "url": "/my_web/static/css/main.5092db0e.chunk.css"
+  },
+  {
+    "revision": "cce28fbeeed2722d1c4039258bbf83a4",
+    "url": "/my_web/index.html"
+  }
+];
