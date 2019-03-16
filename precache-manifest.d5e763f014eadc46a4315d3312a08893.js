7068884050764ecf6bf77adedd94c9508fd140ef
Alex <alexlau@benovelty.com>
2019-03-17 00:37:46 +0800

    Updates

---
+++ precache-manifest.d5e763f014eadc46a4315d3312a08893.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a768861ab71a487cf42b7d3038837f7d",
+    "url": "/my_web/static/media/background-portrait.a768861a.jpg"
+  },
+  {
+    "revision": "978b58317297c20b0ec524927a502c08",
+    "url": "/my_web/static/media/background-landscape.978b5831.jpg"
+  },
+  {
+    "revision": "b11f35f77210c651367969365f87af24",
+    "url": "/my_web/static/media/Trajan Pro Regular.b11f35f7.ttf"
+  },
+  {
+    "revision": "40bacd71a3923f11c248",
+    "url": "/my_web/static/js/runtime~main.40bacd71.js"
+  },
+  {
+    "revision": "44e1ae8e3354b8be3430",
+    "url": "/my_web/static/js/main.44e1ae8e.chunk.js"
+  },
+  {
+    "revision": "21739c29a2eded8f09cc",
+    "url": "/my_web/static/js/2.21739c29.chunk.js"
+  },
+  {
+    "revision": "44e1ae8e3354b8be3430",
+    "url": "/my_web/static/css/main.ecf8c4be.chunk.css"
+  },
+  {
+    "revision": "8c25ea5603a6a5d27c290b20fa91c2eb",
+    "url": "/my_web/index.html"
+  }
+];
