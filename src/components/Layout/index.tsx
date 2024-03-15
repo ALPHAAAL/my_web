f9cc6a85b5b03dfd3f78a7de39b3e673249b1a9e
Alex Lau <cyalexlau@yahoo.com.hk>
2024-03-16 02:13:08 +0800

    Update color scheme (temporary)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,15 +2,15 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div className="h-dvh md:px-[20%] bg-gradient-to-tr to-blue-400 from-green-500">
-            <div className="h-full flex flex-col bg-slate-600 px-5 py-3">
+        <div className="h-dvh md:px-[20%] bg-slate-800 text-green-300">
+            <div className="h-full flex flex-col bg-slate-900 px-5 py-3">
                 <div>
                     Something goes here
                 </div>
                 <div className="grow">
                     <Outlet />
                 </div>
-                <div className="py-3 border-t border-gray-400" />
+                <div className="py-3 border-t border-green-300" />
                 <div className="flex px-5 justify-between">
                     <h1 className="hidden sm:block w-[50%] text-center">The greatest glory in living lies not in never falling, but in rising every time we fall.</h1>
                     <h1 className="w-[50%] text-center">
