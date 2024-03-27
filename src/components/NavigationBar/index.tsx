0c1183fe736d002326ec8a4241994aabf53f8a8c
Alex Lau <cyalexlau@yahoo.com.hk>
2024-03-28 00:35:32 +0800

    Refine navigation bar UI

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NavigationBar() {
     return (
-        <div className={`px-2 sm:flex sm:justify-between hidden fixed bottom-5 left-[50%] ml-[-150px] bg-red-400 w-[300px] h-[50px]`}>
+        <div className={`px-2 sm:flex sm:justify-between items-center align-middle hidden fixed bottom-5 left-[50%] ml-[-150px] bg-red-400 w-[300px] h-[50px]`}>
             <Link to="/about">About</Link>
             <Link to="/about">Gallery</Link>
             <Link to="/about">Blog</Link>
