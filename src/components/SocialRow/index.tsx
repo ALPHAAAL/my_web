ea748d4c916f2cbef52eb9a1c6eb381001127078
Alex Lau <cyalexlau@yahoo.com.hk>
2024-03-31 20:53:50 +0800

    Update social row UI

--- src/components/SocialRow/index.tsx
+++ src/components/SocialRow/index.tsx
@@ -3,16 +3,20 @@ import clsx from 'clsx';
 import GithubIcon from '../../assets/icons/github-mark.svg?react';
 import LinkedinIcon from '../../assets/icons/LinkedIn_icon.svg?react';
 
-export default function SocialRow({ className }: React.ComponentProps<'div'>) {
+type SocialRowPropType = React.ComponentProps<'div'> & {
+    shouldShowName?: boolean;
+}
+
+export default function SocialRow({ className, shouldShowName = true }: SocialRowPropType) {
     return (
-        <div className={clsx(className, 'flex flex-row space-x-2 justify-around h-min')}>
+        <div className={clsx(className, 'flex flex-row space-x-4 justify-around h-min')}>
             <a className='flex hover:text-blue-700' href="https://github.com/ALPHAAAL" target='_blank'>
                 <GithubIcon width={24} height={24} />
-                <span className='ml-3'>Github</span>
+                {shouldShowName && <span className='ml-3'>Github</span>}
             </a>
             <a className='flex hover:text-blue-700' href="https://www.linkedin.com/in/alex-lau-a550a7134/" target='_blank'>
                 <LinkedinIcon width={24} height={24} />
-                <span className='ml-3'>Linkedin</span>
+                {shouldShowName && <span className='ml-3'>Linkedin</span>}
             </a>
         </div>
     );
