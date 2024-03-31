36ef3f355a9e3300cb4ad8add62b98cdcdf71d87
Alex Lau <cyalexlau@yahoo.com.hk>
2024-03-31 18:54:00 +0800

    Update social row styling

--- src/components/SocialRow/index.tsx
+++ src/components/SocialRow/index.tsx
@@ -5,13 +5,13 @@ import LinkedinIcon from '../../assets/icons/LinkedIn_icon.svg?react';
 
 export default function SocialRow({ className }: React.ComponentProps<'div'>) {
     return (
-        <div className={clsx(className, 'flex flex-row space-x-2 justify-around')}>
-            <a className='flex' href="https://github.com/ALPHAAAL" target='_blank'>
-                <GithubIcon className="text-blue-500 hover:text-blue-700" width={24} height={24} />
+        <div className={clsx(className, 'flex flex-row space-x-2 justify-around h-min')}>
+            <a className='flex hover:text-blue-700' href="https://github.com/ALPHAAAL" target='_blank'>
+                <GithubIcon width={24} height={24} />
                 <span className='ml-3'>Github</span>
             </a>
-            <a className='flex' href="https://www.linkedin.com/in/alex-lau-a550a7134/" target='_blank'>
-                <LinkedinIcon className="text-blue-500 hover:text-blue-700" width={24} height={24} />
+            <a className='flex hover:text-blue-700' href="https://www.linkedin.com/in/alex-lau-a550a7134/" target='_blank'>
+                <LinkedinIcon width={24} height={24} />
                 <span className='ml-3'>Linkedin</span>
             </a>
         </div>
