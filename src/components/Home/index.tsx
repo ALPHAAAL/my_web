ee36678838a5ea9169ef6e2e1fd0226afd9007e7
Alex Lau <cyalexlau@yahoo.com.hk>
2024-03-31 20:54:15 +0800

    Home page UI update - add image carousell

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,23 +1,30 @@
 /// <reference types="vite-plugin-svgr/client" />
-import ImageMap from '../../assets/img/hash-source-map'
-
 import LazyImage from '../LazyImage';
 import SocialRow from "../SocialRow";
 
-// import background_white from '../../assets/background_white_code_trim_compressed.mov';
+import { hashToDataUrl } from '../../assets/img/hash-source-map';
+import IMAGE_JSON from '../../assets/img/images.json';
+
+const images = Object.values(IMAGE_JSON).map(({ name, hash }) => {
+    return (
+        <LazyImage key={hash} src={`/assets/images/${name}.webp`} placeholderImage={hashToDataUrl(hash)} className=' max-h-[400px]' />
+    );
+});
+const mobileImages = Object.values(IMAGE_JSON).map(({ name, hash }) => {
+    return (
+        <LazyImage key={hash} src={`/assets/images/${name}.webp`} placeholderImage={hashToDataUrl(hash)} className='object-cover w-full' />
+    );
+});
 
 const TYPING_TEXT_CLASS_NAME = "overflow-hidden whitespace-nowrap"; // TODO: Add this -> `border-r-4 border-r-white pr-2` for blinking cursor
 
 export default function Home() {
     return (
         <div className='h-full'>
-            {/* <video autoPlay muted loop className="absolute w-full h-full object-cover -z-50">
-                <source src={background_white} type="video/mp4" />
-            </video> */}
             <div className="flex flex-col items-start">
                 {/* <h1 className='text-6xl sm:text-8xl font-extrabold bg-clip-text'>Alex Lau</h1> */}
-                <svg height="100" fill="transparent" stroke="currentColor" stroke-width="2" className="stroke-fill-animation text-6xl sm:text-8xl font-bold bg-clip-text" width="100%">
-                    <text dominant-baseline="middle" y="50%">Alex Lau</text>
+                <svg height="100" fill="transparent" stroke="currentColor" strokeWidth="2" className="stroke-fill-animation text-6xl sm:text-8xl font-bold bg-clip-text" width="100%">
+                    <text dominantBaseline="middle" y="50%">Alex Lau</text>
                 </svg>
                 <div className="font-extrabold text-2xl md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200">
                     I am a <span className="text-indigo-500 inline-flex flex-col h-[calc(theme(fontSize.2xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
@@ -30,9 +37,12 @@ export default function Home() {
                         </ul>
                     </span>
                 </div>
-                <SocialRow className='mt-3' />
-                <div className='mt-3 flex flex-col'>
-                    <LazyImage placeholderImage={ImageMap.ProfilePic.hash} src={ImageMap.ProfilePic.src} width={300} height={300} />
+                <SocialRow className='mt-3' shouldShowName={false} />
+                <div className='hidden -translate-x-[15%] w-[calc(100vw)] sm:flex flex-col sm:flex-row mt-3 sm:mt-48 sm:overflow-scroll scroll-smooth scrollbar-hide [&>*]:mr-8'>
+                    {images}
+                </div>
+                <div className='grid grid-cols-2 gap-x-5 gap-y-3 mt-3 sm:hidden'>
+                    {mobileImages}
                 </div>
             </div>
         </div>
