d6af85f07257657321f9d8149a3acc4b34aca06f
Alex Lau <cyalexlau@yahoo.com.hk>
2024-10-25 00:37:49 +0800

    Update navigation bar styling

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NavigationBar() {
     return (
-        <div className="hidden px-4 sm:flex sm:justify-between text-center fixed bottom-5 left-[50%] ml-[-150px] bg-slate-400 dark:bg-stone-400 w-[300px] h-[50px]">
+        <div className="rounded-xl hidden px-4 sm:flex sm:justify-between text-center fixed bottom-5 left-[50%] ml-[-150px] bg-slate-400 dark:bg-stone-400 w-[300px] h-[50px]">
             <Link className="hover:bg-stone-800 w-[33%] h-full leading-[50px]" to="/about">About</Link>
             <Link className="hover:bg-stone-800 w-[33%] h-full leading-[50px]" to="/gallery">Gallery</Link>
             <Link className="hover:bg-stone-800 w-[33%] h-full leading-[50px]" to="/blog">Blog</Link>
