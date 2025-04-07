e56314f417e7e7c79ebddd6472bcc1bb2592dcf4
Alex Lau <cyalexlau@yahoo.com.hk>
2025-04-07 21:47:49 +0800

    Update navigation bar color

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function NavigationBar() {
     return (
-        <div className="rounded-xl hidden px-4 sm:flex sm:justify-between text-center fixed bottom-5 left-[50%] ml-[-150px] bg-slate-400 dark:bg-stone-400 w-[300px] h-[50px]">
-            <Link className="hover:bg-stone-800 w-[33%] h-full leading-[50px]" to="/about">About</Link>
-            <Link className="hover:bg-stone-800 w-[33%] h-full leading-[50px]" to="/gallery">Gallery</Link>
-            <Link className="hover:bg-stone-800 w-[33%] h-full leading-[50px]" to="/blog">Blog</Link>
+        <div className="rounded-xl hidden px-4 sm:flex sm:justify-between text-center fixed bottom-5 left-[50%] ml-[-150px] bg-slate-400 dark:bg-slate-800 border-[1px] dark:border-stone-400 w-[300px] h-[50px]">
+            <Link className="hover:bg-slate-500 dark:hover:bg-slate-700 w-[33%] h-full leading-[50px]" to="/about">About</Link>
+            <Link className="hover:bg-slate-500 dark:hover:bg-slate-700 w-[33%] h-full leading-[50px]" to="/gallery">Gallery</Link>
+            <Link className="hover:bg-slate-500 dark:hover:bg-slate-700 w-[33%] h-full leading-[50px]" to="/blog">Blog</Link>
         </div>
     )
 }
